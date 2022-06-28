22fa1a28ec23470e0c39a2754014a0f372f30075
EmJ <emily@example.com>
2022-06-28 13:06:33 -0700

    saving before trying other style

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,24 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import './static/main.scss';
+import Nav from './Nav';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <div className="body">
+        <header className="header">
+          <img src="./images/avatar.jpg" alt="" />blah
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload and do something.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ portfolio-app/src/Nav.js
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+    return (
+        <nav>
+            stuff
+        </nav>
+    )
+}
+
+export default Nav;
